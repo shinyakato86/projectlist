4207ddc44252bd8f1c42419149943c3235ca0145
shinyakato86 <fujiwarapoteto@gmail.com>
2020-12-13 10:09:36 +0900

    201213
    検索セレクトボックス 修正

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -25,18 +25,22 @@ class SalesController extends Controller
       }else if ($request->filled('seach_year')) {
 
         $keyword = $request->input('seach_year');
+        $keyword2 = $month;
 
         $sales_personal  = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_name , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->groupByRaw('creator_name')->orderBy('amount', 'desc')->get();
 
       }else{
 
-        $sales_personal = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_name , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $year)->whereMonth('accounting_date', $month)->groupByRaw('creator_name')->orderBy('amount', 'desc')->get();
+        $keyword = $year;
+        $keyword2 = $month;
+
+        $sales_personal = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_name , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->whereMonth('accounting_date', $keyword2)->groupByRaw('creator_name')->orderBy('amount', 'desc')->get();
 
         }
 
         $error_text = "検索結果がありません。";
 
-        return view('sales_personal', compact('sales_personal', 'year', 'month', 'error_text'));
+        return view('sales_personal', compact('sales_personal', 'year', 'month', 'error_text', 'keyword', 'keyword2'));
 
     }
 
@@ -58,18 +62,22 @@ class SalesController extends Controller
       }else if ($request->filled('seach_year')) {
 
         $keyword = $request->input('seach_year');
+        $keyword2 = $month;
 
         $sales_client = \DB::table('projectlists')->selectRaw('client_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->groupByRaw('client_name')->orderBy('amount', 'desc')->get();
 
       }else{
 
-        $sales_client = \DB::table('projectlists')->selectRaw('client_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $year)->whereMonth('accounting_date', $month)->groupByRaw('client_name')->orderBy('amount', 'desc')->get();
+        $keyword = $year;
+        $keyword2 = $month;
+
+        $sales_client = \DB::table('projectlists')->selectRaw('client_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->whereMonth('accounting_date', $keyword2)->groupByRaw('client_name')->orderBy('amount', 'desc')->get();
 
       }
 
         $error_text = "検索結果がありません。";
 
-        return view('sales_client', compact('sales_client', 'year', 'month', 'error_text'));
+        return view('sales_client', compact('sales_client', 'year', 'month', 'error_text', 'keyword', 'keyword2'));
     }
 
 
@@ -90,18 +98,22 @@ class SalesController extends Controller
       }else if ($request->filled('seach_year')) {
 
         $keyword = $request->input('seach_year');
+        $keyword2 = $month;
 
         $sales_category = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_category , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->groupByRaw('creator_category')->orderBy('amount', 'desc')->get();
 
       }else{
 
-        $sales_category = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_category , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $year)->whereMonth('accounting_date', $month)->groupByRaw('creator_category')->orderBy('amount', 'desc')->get();
+        $keyword = $year;
+        $keyword2 = $month;
+
+        $sales_category = \DB::table('projectlists')->join('creators', 'projectlists.id', '=', 'creators.id')->selectRaw('creator_category , sum(creator_price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->whereMonth('accounting_date', $keyword2)->groupByRaw('creator_category')->orderBy('amount', 'desc')->get();
 
         }
 
         $error_text = "検索結果がありません。";
 
-        return view('sales_category', compact('sales_category', 'year', 'month', 'error_text'));
+        return view('sales_category', compact('sales_category', 'year', 'month', 'error_text', 'keyword', 'keyword2'));
 
     }
 
@@ -123,18 +135,22 @@ class SalesController extends Controller
       }else if ($request->filled('seach_year')) {
 
         $keyword = $request->input('seach_year');
+        $keyword2 = $month;
 
         $sales_department = \DB::table('projectlists')->selectRaw('department_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->groupByRaw('department_name')->orderBy('amount', 'desc')->get();
 
       }else{
 
-        $sales_department = \DB::table('projectlists')->selectRaw('department_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $year)->whereMonth('accounting_date', $month)->groupByRaw('department_name')->orderBy('amount', 'desc')->get();
+        $keyword = $year;
+        $keyword2 = $month;
+
+        $sales_department = \DB::table('projectlists')->selectRaw('department_name , sum(price) as amount, accounting_date')->whereYear('accounting_date', $keyword)->whereMonth('accounting_date', $keyword2)->groupByRaw('department_name')->orderBy('amount', 'desc')->get();
 
         }
 
         $error_text = "検索結果がありません。";
 
-        return view('sales_department', compact('sales_department', 'year', 'month', 'error_text'));
+        return view('sales_department', compact('sales_department', 'year', 'month', 'error_text', 'keyword', 'keyword2'));
 
     }
 
--- resources/views/search-02.blade.php
+++ resources/views/search-02.blade.php
@@ -5,7 +5,7 @@
           <select class="form-control" name="seach_year">
               <option value="" selected>年</option>
               @for ($i=$year - 5; $i <= $year; $i++)
-              <option value="{{$i}}" @if($i == $year) selected="selected" @endif>{{$i}}年</option>
+              <option value="{{$i}}" @if($i == $keyword) selected="selected" @endif>{{$i}}年</option>
               @endfor
           </select>
         </div>
@@ -13,7 +13,7 @@
           <select class="form-control" name="seach_month">
               <option value="">月</option>
               @for ($i=1; $i <= 12; $i++)
-              <option value="{{ $i }}" @if($i == $month) selected="selected" @endif>{{ $i }}月</option>
+              <option value="{{ $i }}" @if($i == $keyword2) selected="selected" @endif>{{ $i }}月</option>
               @endfor
           </select>
         </div>
